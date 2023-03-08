2dbc8f08fadc69d8da1ffe7b4f27581b6733bdde
BLUE <89306062+BLVEhv@users.noreply.github.com>
2023-03-09 00:35:54 +0700

    setState and DOM

--- src/view/Example/MyComponent.js
+++ src/view/Example/MyComponent.js
@@ -2,17 +2,31 @@ import React from "react";
 
 class MyComponent extends React.Component {
     state = {
-        name: "Hieu",
+        name: "",
         nickName: "BLUE"
+    };
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        });
+    };
+
+    handleClickButton = () => {
+        alert("Happy new year!");
     };
     render() {
         return (
             <>
                 <div>
-                    My name is {this.state.name}
+                    <input value={this.state.name} type="text" onChange={(event) => { this.handleOnChangeName(event); }}></input>
+                    My name is {this.state['name']}
                 </div>
                 <div>
                     My nick name is {this.state.nickName}
+                </div>
+                <div>
+                    <button onClick={this.handleClickButton} >Click me</button>
                 </div>
             </>
         );
